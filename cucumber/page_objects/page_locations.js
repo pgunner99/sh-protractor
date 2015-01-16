34d9879a4e001419ce826bc5633ba82bf660a6ea
Pete Richardson <peter@brightstreetgroup.com>
2015-01-16 14:22:03 -0500

    added page_objects

---
+++ cucumber/page_objects/page_locations.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var PageLocations = function() {
+  this.create_new_group = "https://wwwtest.internal.priorityhealth.com/app/priority-quote-beta/#/new-group";
+  this.priorityquote_homepage = "https://wwwtest.internal.priorityhealth.com/app/priority-quote-beta/#/login?returnUrl=%2Flogin"
+  this.priorityquote_individual = "https://wwwtest.internal.priorityhealth.com/app/priority-quote-beta/#/quotes/census/individual"
+ 
+};
+
+module.exports = PageLocations;
+
