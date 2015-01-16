de9b7a165af26e1012af79c0c73e69051ab853f9
Pete Richardson <peter@brightstreetgroup.com>
2015-01-16 14:19:15 -0500

    update

---
+++ cucumber/support/support.js
@@ -0,0 +1,18 @@
+var Support = function(){
+};
+
+Support.prototype.logIntoPriorityQuote = function(user, password, callback){
+    browser.get("app/priority-quote-beta/#/login?returnUrl=%2Flogin");
+    element(by.name("username")).sendKeys(user);
+    element(by.name("password")).sendKeys(password);
+    browser.driver.sleep(2000).then( function() {
+        element(by.css("input.ph-btn.btn-green")).click();
+        console.log("After Click");
+        browser.driver.sleep(10000).then(function(result) {
+            console.log("After Sleep");
+            callback(result);
+        });
+    });
+};
+
+module.exports = new Support();
